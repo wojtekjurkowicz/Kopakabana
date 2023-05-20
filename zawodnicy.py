4aa790446084b1334e72f72e8b493afef7c57e96
wojtekjurkowicz <wojtek.jurkowicz@gmail.com>
2023-05-20 14:54:26 +0200

    poprawione klasy Druzyna i Zawodnik :)

--- zawodnicy.py
+++ zawodnicy.py
@@ -8,34 +8,35 @@ class Zawodnik:
 
 
 class Druzyna:
-    def __init__(self, nazwa, dyscyplina):
+    def __init__(self, nazwa):
         self.nazwa = nazwa
-        self.dyscyplina = dyscyplina
-        self.liczba_zawodnikow = 0
         self.lista_zawodnikow = []
-        self.punkty = 0
+        self.punkty = {
+            "siatkowka_plazowa": 0,
+            "dwa_ognie": 0,
+            "przeciaganie_liny": 0
+        }
 
     def zglos_zawodnika(self, zawodnik):
-        if self.liczba_zawodnikow < 6:
+        if len(self.lista_zawodnikow) <= 6:
             self.lista_zawodnikow.append(zawodnik)
-            self.liczba_zawodnikow += 1
         else:
             return False
 
     def wycofaj_zawodnika(self, zawodnik):
-        if self.liczba_zawodnikow > 0:
+        if len(self.lista_zawodnikow) > 0:
             if zawodnik in self.lista_zawodnikow:
                 self.lista_zawodnikow.remove(zawodnik)
-                self.liczba_zawodnikow -= 1
             else:
                 return f"Nie ma takiego zawodnika jak {zawodnik}"
 
-    def dodaj_punkt(self):
-        self.punkty += 1
-    def przeglad_druzyny(self):
-        return f"{self.nazwa}({self.dyscyplina}): {self.lista_zawodnikow}"
+    def dodaj_punkt(self, dyscyplina):
+        self.punkty[dyscyplina] += 1
 
-druzyna = Druzyna("FC Po Nalewce", "siatkowka_plazowa")
+    def przeglad_druzyny(self):
+        return f"{self.nazwa}: {self.lista_zawodnikow}"
+
+druzyna = Druzyna("FC Po Nalewce")
 druzyna.zglos_zawodnika("Wojtek Jurkowicz")
 druzyna.zglos_zawodnika("Adam Kowalski")
 druzyna.zglos_zawodnika("Adam Nowak")
@@ -45,3 +46,7 @@ druzyna.zglos_zawodnika("Andrzej Kowal")
 druzyna.wycofaj_zawodnika("Adam Nowak")
 druzyna.zglos_zawodnika("Mateusz Ziom")
 print(druzyna.przeglad_druzyny())
+print(druzyna.punkty)
+druzyna.dodaj_punkt("dwa_ognie")
+druzyna.dodaj_punkt("dwa_ognie")
+print(druzyna.punkty)
