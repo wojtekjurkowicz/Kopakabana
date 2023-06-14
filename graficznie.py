6db2a190d3f2cad2496ae007a74574635e39109d
wojtekjurkowicz <wojtek.jurkowicz@gmail.com>
2023-06-14 16:08:20 +0200

    zrob

--- graficznie.py
+++ graficznie.py
@@ -5,49 +5,38 @@ import random
 
 glowne_okno = Tk()
 glowne_okno.resizable(width=False, height=False)
-
 glowne_okno.title("Rozgrywki na słonecznej plaży Kopakabana")
 glowne_okno.iconbitmap("icon.ico")
 glowne_okno.geometry("800x600")
-# bg = PhotoImage(file="kopakabana.ppm")
-# image_label = Label(glowne_okno, image=bg)
-# image_label.place(x=0, y=0)
 
 frameCnt = 12
-frames = [PhotoImage(file='kopakabana.gif', format = 'gif -index %i' %(i)) for i in range(frameCnt)]
+frames = [PhotoImage(file='kopakabana.gif', format = 'gif -index %i' % i) for i in range(frameCnt)]
 
 def update(ind):
     frame = frames[ind]
     ind += 1
     if ind == frameCnt:
         ind = 0
-    label.configure(image=frame)
+    gif.configure(image=frame)
     glowne_okno.after(100, update, ind)
 
+gif = Label(glowne_okno)
+gif.pack()
+glowne_okno.after(0, update, 0)
 
 def losuj_sedziow():
-    frameCnt = 12
-    frames = [PhotoImage(file='kopakabana1.gif', format='gif -index %i' % (i)) for i in range(frameCnt)]
+    gif.destroy()
+    bg = PhotoImage(file="kopakabana.png")
+    bg_label = Label(glowne_okno, image=bg)
+    bg_label.place(x=0, y=0)
 
-    def update(ind):
-        frame = frames[ind]
-        ind += 1
-        if ind == frameCnt:
-            ind = 0
-        label.configure(image=frame)
-        glowne_okno.after(100, update, ind)
-
-    label = Label(glowne_okno)
-    label.pack()
-    glowne_okno.after(0, update, 0)
-
-    button.destroy()
-    button1.destroy()
-    button2.destroy()
-    f = open('sedziowie.txt').read()
-    f = f.split("\n")
+    siatkowka_przycisk.destroy()
+    dwa_ognie_przycisk.destroy()
+    przeciaganie_liny_przycisk.destroy()
+    plik = open('sedziowie.txt').read()
+    plik = plik.split("\n")
     lista_sedziow = []
-    for i in f:
+    for i in plik:
         lista_sedziow.append(i)
 
     sedziowie = Sedziowie()
@@ -57,23 +46,79 @@ def losuj_sedziow():
         lista_sedziow.remove(sedzia)
 
     print(sedziowie)
-    label = Label(glowne_okno, text="Sędziowie zostali wylosowani z puli\n\nCzy chcesz zmodyfikować listę?", font=("Arial", 15))
-    label.place(x=230, y=100)
+    lista = []
+    lista_labeli = []
 
+    def wypisz_sedziow():
+        d_y = 150
+        for sedzia in sedziowie.lista_sedziow:
+            sedzia_label = Label(glowne_okno, text=sedzia)
+            lista.append(sedzia)
+            lista_labeli.append(sedzia_label)
+            sedzia_label.place(x=450, y=d_y)
+            d_y += 25
+
+    komunikat = Label(glowne_okno, text="Lista sędziów: ", font=("Arial", 15))
+    komunikat.place(x=450, y=100)
+    wypisz_sedziow()
+
+    pytanie = Label(glowne_okno, text="Czy chcesz zmodyfikować listę? (max: 10)")
+    pytanie.place(x=100, y=450)
     imie_label = Label(glowne_okno, text="Imię: ")
-    imie_label.place(x=100, y=250)
+    imie_label.place(x=100, y=475)
     imie_field = Entry(glowne_okno)
-    imie_field.place(x=200, y=250)
+    imie_field.place(x=175, y=475)
     nazwisko_label = Label(glowne_okno, text="Nazwisko: ")
-    nazwisko_label.place(x=100, y=300)
+    nazwisko_label.place(x=100, y=500)
     nazwisko_field = Entry(glowne_okno)
-    nazwisko_field.place(x=200, y=300)
-    dodaj = Button(glowne_okno, text="Dodaj", height=2, width=14, command=sedziowie.dodaj_sedziego(Sedzia(imie_field, nazwisko_field)))
-    dodaj.place(x=100, y=350)
-    usun = Button(glowne_okno, text="Usuń", height=2, width=14, command=sedziowie.dodaj_sedziego(Sedzia(imie_field, nazwisko_field)))
-    usun.place(x=200, y=350)
+    nazwisko_field.place(x=175, y=500)
+
+    nie_dla_sedziow = Label(glowne_okno, text="Nie możesz dodać więcej sędziów!")
+    def dopisz_sedziego():
+        if len(lista) <= 19:
+            imie = imie_field.get()
+            nazwisko = nazwisko_field.get()
+            sedzia = Sedzia(imie, nazwisko)
+            sedziowie.dodaj_sedziego(Sedzia(imie, nazwisko))
+            print(f"imie: {imie}, nazwisko: {nazwisko}")
+            sedzia_label = Label(glowne_okno, text=str(sedzia))
+            lista_labeli.append(sedzia_label)
+            lista.append(sedzia)
+            sedzia_label.place(x=600, y=150+(len(lista)-11)*25)
+        else:
+            pytanie.destroy()
+            nie_dla_sedziow.place(x=100, y=450)
+            dodaj.destroy()
+
+    def usun_sedziego():
+        if len(lista) >= 5:
+            imie = imie_field.get()
+            nazwisko = nazwisko_field.get()
+            sedzia = Sedzia(imie, nazwisko)
+            print(f"imie: {imie}, nazwisko: {nazwisko}")
+            if sedzia in lista:
+                ind = lista.index(sedzia)
+                sedziowie.usun_sedziego(sedzia)
+                lista.remove(sedzia)
+                sedzia_label = lista_labeli[ind]
+                sedzia_label.destroy()
+                lista_labeli.remove(lista_labeli[ind])
+                wypisz_sedziow()
+            else:
+                nie_ma = Label(glowne_okno, text="Nie ma takiego sędziego na liście!")
+
+
+    dodaj = Button(glowne_okno, text="Dodaj", height=2, width=12, command=dopisz_sedziego)
+    dodaj.place(x=100, y=525)
+    usun = Button(glowne_okno, text="Usuń", height=2, width=12, command=usun_sedziego)
+    usun.place(x=175, y=525)
+
+
+
     def przejdz_dalej():
-        label.destroy()
+        nie_dla_sedziow.destroy()
+        komunikat.destroy()
+        pytanie.destroy()
         imie_label.destroy()
         imie_field.destroy()
         nazwisko_label.destroy()
@@ -81,17 +126,23 @@ def losuj_sedziow():
         dodaj.destroy()
         usun.destroy()
         dalej.destroy()
+
+        for sedzia in lista_labeli:
+            sedzia.destroy()
+
         return sedziowie
 
     dalej = Button(glowne_okno, text="Przejdź dalej", height=5, width=20, command=przejdz_dalej)
-    dalej.place(x=500, y=250)
+    dalej.place(x=525, y=475)
+
 
 def siatkowka_plazowa():
     sedziowie = losuj_sedziow()
+
     rozgrywki = Siatkowka_plazowa(sedziowie.lista_sedziow)
-    label = Label(glowne_okno, text="Dodaj, usuń lub wygeneruj drużyny do rozpoczęcia rozgrywek!",
+    druzyny_label = Label(glowne_okno, text="Dodaj, usuń lub wygeneruj drużyny do rozpoczęcia rozgrywek!",
                   font=("Arial", 15))
-    label.place(x=230, y=100)
+    druzyny_label.place(x=230, y=100)
     def dodawanie_druzyny():
         nazwa_label = Label(glowne_okno, text="Nazwa drużyny: ",
                       font=("Arial", 15))
@@ -120,30 +171,105 @@ def siatkowka_plazowa():
                       font=("Arial", 15))
         label.place(x=230, y=400)
 
+    # if not (komunikat and pytanie and imie_label and imie_field and nazwisko_label and nazwisko_field and dodaj and usun and dalej):
     dodaj = Button(glowne_okno, text="Dodaj drużynę", height=5, width=20, command=dodawanie_druzyny)
     dodaj.place(x=150, y=250)
-    generuj = Button(glowne_okno, text="Wygeneruj drużynę", height=5, width=20, command=przejdz_dalej)
+    generuj = Button(glowne_okno, text="Wygeneruj drużynę", height=5, width=20)
+    generuj.place(x=450, y=250)
+
+def dwa_ognie():
+    sedziowie = losuj_sedziow()
+
+    rozgrywki = Siatkowka_plazowa(sedziowie.lista_sedziow)
+    druzyny_label = Label(glowne_okno, text="Dodaj, usuń lub wygeneruj drużyny do rozpoczęcia rozgrywek!",
+                  font=("Arial", 15))
+    druzyny_label.place(x=230, y=100)
+    def dodawanie_druzyny():
+        nazwa_label = Label(glowne_okno, text="Nazwa drużyny: ",
+                      font=("Arial", 15))
+        nazwa_label.place(x=100, y=300)
+        nazwa_field = Entry(glowne_okno)
+        nazwa_field.place(x=200, y=325)
+        label = Label(glowne_okno, text="Zgłoś zawodnika do drużyny",
+                      font=("Arial", 15))
+        label.place(x=100, y=300)
+        imie_label = Label(glowne_okno, text="Imię: ")
+        imie_label.place(x=100, y=325)
+        imie_field = Entry(glowne_okno)
+        imie_field.place(x=200, y=325)
+        nazwisko_label = Label(glowne_okno, text="Nazwisko: ")
+        nazwisko_label.place(x=100, y=350)
+        nazwisko_field = Entry(glowne_okno)
+        nazwisko_field.place(x=200, y=350)
+        dodaj = Button(glowne_okno, text="Dodaj", height=2, width=14,
+                       command=(Sedzia(imie_field, nazwisko_field)))
+        dodaj.place(x=100, y=350)
+        usun = Button(glowne_okno, text="Usuń", height=2, width=14,
+                      command=sedziowie.dodaj_sedziego(Sedzia(imie_field, nazwisko_field)))
+        usun.place(x=200, y=350)
+
+        label = Label(glowne_okno, text="Usuń zawodnika z drużyny",
+                      font=("Arial", 15))
+        label.place(x=230, y=400)
+
+    # if not (komunikat and pytanie and imie_label and imie_field and nazwisko_label and nazwisko_field and dodaj and usun and dalej):
+    dodaj = Button(glowne_okno, text="Dodaj drużynę", height=5, width=20, command=dodawanie_druzyny)
+    dodaj.place(x=150, y=250)
+    generuj = Button(glowne_okno, text="Wygeneruj drużynę", height=5, width=20)
+    generuj.place(x=450, y=250)
+
+
+def przeciaganie_liny():
+    sedziowie = losuj_sedziow()
+
+    rozgrywki = Siatkowka_plazowa(sedziowie.lista_sedziow)
+    druzyny_label = Label(glowne_okno, text="Dodaj, usuń lub wygeneruj drużyny do rozpoczęcia rozgrywek!",
+                  font=("Arial", 15))
+    druzyny_label.place(x=230, y=100)
+    def dodawanie_druzyny():
+        nazwa_label = Label(glowne_okno, text="Nazwa drużyny: ",
+                      font=("Arial", 15))
+        nazwa_label.place(x=100, y=300)
+        nazwa_field = Entry(glowne_okno)
+        nazwa_field.place(x=200, y=325)
+        label = Label(glowne_okno, text="Zgłoś zawodnika do drużyny",
+                      font=("Arial", 15))
+        label.place(x=100, y=300)
+        imie_label = Label(glowne_okno, text="Imię: ")
+        imie_label.place(x=100, y=325)
+        imie_field = Entry(glowne_okno)
+        imie_field.place(x=200, y=325)
+        nazwisko_label = Label(glowne_okno, text="Nazwisko: ")
+        nazwisko_label.place(x=100, y=350)
+        nazwisko_field = Entry(glowne_okno)
+        nazwisko_field.place(x=200, y=350)
+        dodaj = Button(glowne_okno, text="Dodaj", height=2, width=14,
+                       command=(Sedzia(imie_field, nazwisko_field)))
+        dodaj.place(x=100, y=350)
+        usun = Button(glowne_okno, text="Usuń", height=2, width=14,
+                      command=sedziowie.dodaj_sedziego(Sedzia(imie_field, nazwisko_field)))
+        usun.place(x=200, y=350)
+
+        label = Label(glowne_okno, text="Usuń zawodnika z drużyny",
+                      font=("Arial", 15))
+        label.place(x=230, y=400)
+
+    # if not (komunikat and pytanie and imie_label and imie_field and nazwisko_label and nazwisko_field and dodaj and usun and dalej):
+    dodaj = Button(glowne_okno, text="Dodaj drużynę", height=5, width=20, command=dodawanie_druzyny)
+    dodaj.place(x=150, y=250)
+    generuj = Button(glowne_okno, text="Wygeneruj drużynę", height=5, width=20)
     generuj.place(x=450, y=250)
 
 
 
+siatkowka_przycisk = Button(glowne_okno, height=3, width=25, text="Siatkówka plażowa", command=siatkowka_plazowa)
+siatkowka_przycisk.place(x=100, y=450)
 
+dwa_ognie_przycisk = Button(glowne_okno, height=3, width=25, text="Dwa ognie", command=dwa_ognie)
+dwa_ognie_przycisk.place(x=300, y=450)
 
-label = Label(glowne_okno)
-label.pack()
-glowne_okno.after(0, update, 0)
-
-button = Button(glowne_okno, height=3, width=25, text="Siatkówka plażowa", command=losuj_sedziow)
-button.place(x=100, y=450)
-
-button1 = Button(glowne_okno, height=3, width=25, text="Dwa ognie", command=losuj_sedziow)
-button1.place(x=300, y=450)
-
-button2 = Button(glowne_okno, height=3, width=25, text="Przeciąganie liny", command=losuj_sedziow)
-button2.place(x=500, y=450)
-
-# def siatkowka_graficznie():
-
+przeciaganie_liny_przycisk = Button(glowne_okno, height=3, width=25, text="Przeciąganie liny", command=przeciaganie_liny)
+przeciaganie_liny_przycisk.place(x=500, y=450)
 
 glowne_okno.mainloop()
 
